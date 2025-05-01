5a192377132386be551e366c210bca9bf1479f98
samarbakhsh77 <samarbakhsh77@gmail.com>
2025-05-01 10:07:10 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,15 @@
 import streamlit as st
+import pickle
+import numpy as np
+model = pickle.load(open('trained_model.pkl', 'rb') )
+st.title("Iris Prediction")
+sepal_length = st.number_input('Sepal Length')
+sepal_width = st.number_input('Sepal width')
+petal_length = st.number_input('Petal Length')
+petal_width = st.number_input('Petal width')
 
-st.title('🎈 App Name')
-
-st.write('Hello world!')
+input_data = [sepal_length,sepal_width, petal_length,   petal_width]
+input_data = np.asarray(input_data).reshape(1, -1)
+if st.button('Result'):
+    prediction = model.predict(input_data)
+    st.success(prediction[0])
